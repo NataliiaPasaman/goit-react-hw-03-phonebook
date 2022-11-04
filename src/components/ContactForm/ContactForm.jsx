f3618a8972c501b7bdc041239885834ae4aa1405
NataliiaPasaman <natapasaman@gmail.com>
2022-11-04 10:46:25 +0200

    fixed reset in ContactsForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -39,6 +39,7 @@ export class ContactForm extends Component {
             className={css.inputPhone}
             type="text"
             name="name"
+            value={this.state.name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -51,6 +52,7 @@ export class ContactForm extends Component {
             className={css.inputPhone}
             type="tel"
             name="number"
+            value={this.state.number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
